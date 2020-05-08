69bb4065dd8c3e101ee3a4f97e314361063e5db2
Salim3dd <salim3ddoman@gmail.com>
2020-05-08 07:17:08 +0400

    Add files via upload

---
+++ GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleMobileAdsDemoScript : MonoBehaviour
+{
+    private BannerView bannerView;
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+         this.RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        this.bannerView.LoadAd(request);
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
